8a479f0884184b13a0a723e569446d45b1619f6d
Paloma Araujo <paclaraujo@gmail.com>
2024-05-17 13:02:33 -0300

    feat: api for taxis

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const swaggerDocument = require('./swagger.json');
 
 const taxisRouter = require('./routes/taxis');
 const swagger = swaggerjsdoc({definition: swaggerDocument, apis: ["./routes/*.js"],});
-const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.css";
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
 app.use(express.static(pathToSwaggerUi));
 
